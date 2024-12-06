9ea0a2e66dffeea12e15898717c3db0de4fb98eb
semkufu95 <semkufu95@gmail.com>
2024-12-06 03:44:30 +0300

    Algorithm dealing with 2D arrays

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A module to calculate the perimeter of an island
+"""
+
+
+def island_perimeter(grid):
+    """
+    Function to calculate perimeter of an island
+    Args: grid (The perimeter of whole area)
+    """
+    perimeter = 0
+    height = len(grid)
+    width = len(grid[0]) if height > 0 else 0
+
+    for height in range(height):
+        for width in range(width):
+            if grid[height][width] == 1:
+                perimeter += 4
+
+                # Check cells for row and columns
+                if height > 0 and grid[height - 1][width] == 1:
+                    perimeter -= 1
+                if height < height - 1 and grid[height + 1][width] == 1:
+                    perimeter -= 1
+                if width > 0 and grid[height][width - 1] == 1:
+                    perimeter -= 1
+                if width < width - 1 and grid[height][width + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
