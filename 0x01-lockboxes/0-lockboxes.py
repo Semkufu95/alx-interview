00c249e83d971923a2b882f62e191ded93bf656f
semkufu95 <semkufu95@gmail.com>
2024-12-06 05:23:24 +0300

    Determine unlocked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be unlocked.
+
+    Args:
+        boxes (list of list of int): List where each index represents a box
+    and contains a list of keys available in that box.
+
+    Returns:
+        bool: True if all boxes can be unlocked, otherwise False.
+    """
+    n = len(boxes)
+    unlocked = [False] * n  # Keep track of unlocked boxes
+    unlocked[0] = True      # The first box is unlocked
+    keys = set(boxes[0])    # Collect all keys from the first box
+
+    while True:
+        new_key_found = False
+        for i in range(n):
+            if not unlocked[i] and i in keys:
+                unlocked[i] = True
+                keys.update(boxes[i])
+                new_key_found = True
+        if not new_key_found:
+            break
+
+    return all(unlocked)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,3 @@
+LOCKBOXES
+
+An algorithm to determimne if boxes can be unlocked
